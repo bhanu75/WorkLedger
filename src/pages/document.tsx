f8a16f02c10cd1b44607add50efff4c7496cdc8a
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-08-20 11:49:13 +0530

    Create document.tsx

---
+++ src/pages/document.tsx
@@ -0,0 +1,20 @@
+
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Simple and reliable time tracking app" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Time Tracker" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
